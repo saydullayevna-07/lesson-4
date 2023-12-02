f99c46c313c97ef2c32c0801c008362d143b7fcc
saydullayevna-07 <rejabboyevamadina6@gmail.com>
2023-12-02 16:01:03 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 1-vazifa
+
+a = 5621557700
+b = int(input("2 dan 10 gacha son kiriting: "))
+if a % b:
+    print("noto'g'ri")
+else:
+    print("to'g'ri")
+
+c = 266
+b = int(input("2 dan 7 gacha son kiriting: "))
+if c % b:
+   print("noto'g'ri")
+else:
+   print("to'g'ri")
+
+# 2-vazifa
+
+d = int(input("olma:"))
+c = int(input(" odam: "))
+print(d - c)
